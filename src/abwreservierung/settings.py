51ff703da1d1dc6b1d9307dd392cffbda3ad885d
holytortoise <nick.white@outlook.de>
2017-09-16 09:00:38 +0200

    add host

--- src/abwreservierung/settings.py
+++ src/abwreservierung/settings.py
@@ -34,9 +34,9 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.11']
 
 
 # Application definition
