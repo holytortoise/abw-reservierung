567f904ef9e279233e8160ab2fc49a77c8df0be5
holytortoise <nick.white@outlook.de>
2017-09-16 17:42:08 +0200

    add update functionality

---
+++ auto_process.py
@@ -0,0 +1,22 @@
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def update():
+    """
+    Automatisches Update des Reservierungssystems über Github
+    """
+    os.system('sudo supervisorctl stop abwreservierung && git pull -b development https://github.com/holytortoise/abwreservierung.git && sudo supervisorctl start abwreservierung')
+
+def alte_reservierungen():
+    """
+    Automatisches entfernen der alten Reservierungen
+    """
+    os.system('cd src && python manage.py alte_reservierungen')
+
+s_update = BlockingScheduler()
+s_update.add_job(update, 'cron', day_of_week='sun', hour=12)
+s_update.start()
+
+s_alt = BlockingScheduler()
+s_alt.add_job(alte_reservierungen, 'cron', day_of_week="mon-fri", hour=17)
+s_alt.start()
---
+++ manual_update.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('sudo supervisorctl stop abwreservierung && git pull -b development https://github.com/holytortoise/abwreservierung.git && sudo supervisorctl start abwreservierung'))
