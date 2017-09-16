c076a4f153be078b219b75a207d3d7f76813e452
holytortoise <nick.white@outlook.de>
2017-09-16 17:46:54 +0200

    test

--- manual_update.py
+++ manual_update.py
@@ -1,3 +1,6 @@
 import os
+from auto_process import alte_reservierungen
 
 os.system('sudo supervisorctl stop abwreservierung && git pull && sudo supervisorctl start abwreservierung')
+
+alte_reservierungen()
