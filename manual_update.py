0dd9b19e28ebe4960a8007eacd15aae2950ab463
holytortoise <nick.white@outlook.de>
2017-09-16 19:05:21 +0200

    add chmod

--- manual_update.py
+++ manual_update.py
@@ -1,6 +1,4 @@
 import os
 from auto_process import alte_reservierungen
 
-os.system('sudo supervisorctl stop abwreservierung && git pull && sudo supervisorctl start abwreservierung')
-
-alte_reservierungen()
+os.system('sudo supervisorctl stop abwreservierung && git pull && sudo supervisorctl start abwreservierung && sudo chmod +x startup')
