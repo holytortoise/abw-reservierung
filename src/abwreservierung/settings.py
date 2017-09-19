3888449d8e93f7e6e89304160666dd8b5ad19157
holytortoise <holytortoise@users.noreply.github.com>
2017-09-19 08:01:08 +0200

    change allowed hosts

--- src/abwreservierung/settings.py
+++ src/abwreservierung/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.11']
+ALLOWED_HOSTS = ['192.168.0.11','172.16.32.192','abw.local']
 
 
 # Application definition
