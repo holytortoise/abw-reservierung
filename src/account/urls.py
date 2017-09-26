cadde11db77108cff26bce953dc3b17ec3350b03
holytortoise <holytortoise@users.noreply.github.com>
2017-09-26 09:44:46 +0200

    change order

--- src/account/urls.py
+++ src/account/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     # Predefined Views
     url(r'login/$', auth_views.LoginView.as_view(template_name="account/login.html"), name='login'),
     url(r'logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url('^password_change/done/$',
+     auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"),
+      name='password-change-done'),
     url('^password_change/$',
      auth_views.PasswordChangeView.as_view(template_name="account/password_change.html",
      success_url="account:password-change-done"),
       name='password-change'),
-    url('^password_change/done/$',
-     auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"),
-      name='password-change-done'),
+
 ]
