5ea46344af42d39cbc912658ad89f8f8a7aa1924
holytortoise <holytortoise@users.noreply.github.com>
2017-09-26 09:30:22 +0200

    change password

--- src/account/urls.py
+++ src/account/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     url(r'login/$', auth_views.LoginView.as_view(template_name="account/login.html"), name='login'),
     url(r'logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url('^password_change/$',
-     auth_views.PasswordChangeView.as_view(template_name="account/password_change.html"),
+     auth_views.PasswordChangeView.as_view(template_name="account/password_change.html",
+     success_url="account:password-change-done"),
       name='password-change'),
     url('^password_change/done/$',
-     auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html",
-     success_url="account:password-change-done"), name='password-change-done'),
+     auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"),
+      name='password-change-done'),
 ]
