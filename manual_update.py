018992d95b97a6f4e2656466dc2a6a7cfeacc9b9
holytortoise <holytortoise@users.noreply.github.com>
2017-09-28 15:44:44 +0200

    change

--- manual_update.py
+++ manual_update.py
@@ -1,3 +1,20 @@
 import os
+from decouple import config
 
-os.system('sudo supervisorctl stop abwreservierung && git pull && sudo supervisorctl start abwreservierung && sudo chmod +x startup')
+sudoPassword = config('SUDOPASSWORD')
+
+def update():
+    """
+    Automatisches Update des Reservierungssystems über Github
+    """
+    command = 'sudo supervisorctl stop abwreservierung && cd /home/webserver/abwreservierung && git pull && sudo supervisorctl start abwreservierung && sudo chmod +x startup'
+    os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+
+def alte_reservierungen():
+    """
+    Automatisches entfernen der alten Reservierungen
+    """
+    os.system("/bin/bash -c 'source ../django-server/bin/activate && python src/manage.py alte_reservierungen && deactivate'")
+
+update()
+alte_reservierungen()
