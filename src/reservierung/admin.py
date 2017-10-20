61fc87466834f9d16ab75b7aa30f5abe59923179
holytortoise <holytortoise@users.noreply.github.com>
2017-10-20 09:08:38 +0200

    Update admin.py

--- src/reservierung/admin.py
+++ src/reservierung/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Raum, Reservierung
 # Register your models here.
 admin.site.register(Raum)
-admin.site.register(Reservierung)
+#admin.site.register(Reservierung)
